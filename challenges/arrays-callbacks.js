c07e683836c14ef152a501f4517176e9cf830473
Tharshan Ketheeswaran <tketheeswaran09@gmail.com>
2020-05-22 18:31:01 +0100

    Done sprint!!

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -8,27 +8,55 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
+const tyran = {
+  name: "tyrannosaurus",
+  diet: "carnivorous",
+  weight: "7000kg",
+  lenght: "12m",
+  period: "Late Cretaceous",
+  roar: function () {
+    return "RAWERSRARARWERSARARARRRR!";
+  }
+}
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const stego = {
+  name: "stegosaurus",
+  diet: "herbivorous",
+  weight: "2000kg",
+  lenght: "9m",
+  period: "Late Jurassic"
+}
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+const velo = {
+  name: "velociraptor",
+  diet: "carnivorous",
+  weight: "15kg",
+  lenght: "1.8m",
+  period: "Late Cretaceous"
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyran.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velo.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stego.lenght);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyran.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyran.roar())
+
 
 
 // ==== Arrays ====
@@ -52,7 +80,8 @@ const graduates = [
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities);
+graduates.map(object => universities.push(object.university))
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -61,10 +90,11 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
+graduates.forEach(object => contactInfo.push(`graduates:${object.first_name} ${object.email}`));
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
+const unisWithUni = graduates.filter(n => n.university.includes("Uni"));
 console.log(unisWithUni);
 
 
@@ -91,6 +121,8 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach(n => displayNames.push(`animal_name: ${n.animal_name}: 
+scientific_name: ${n.scientific_name}`));
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -100,6 +132,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames = [];
+zooAnimals.map(n => lowCaseAnimalNames.push(n.animal_name.toLowerCase()));
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -107,15 +140,22 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+const lowPopulationAnimals = zooAnimals.filter(n => n.population < 5);
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+The zoos need to know their total animal population across the United States. 
+Find the total population from all the zoos using the .reduce() method. 
+Remember the reduce method takes two arguments: a callback (which itself takes two args), 
+and an initial value for the count.
 
 */
-const populationTotal = 0;
+let populationTotal = 0;
+zooAnimals.reduce(function (totalAnimals, zooAnimal) {
+  return totalAnimals +
+    zooAnimal.population
+}, 0);
 console.log(populationTotal);
 
 
--- challenges/closure.js
+++ challenges/closure.js
@@ -1,4 +1,4 @@
-// ==== Callbacks ====  
+// ==== Closure ====  
 
 /* Step 1: Create a higher-order function
   * Create a higher-order function named consume with 3 parameters: a, b and cb
@@ -7,6 +7,10 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(param1, param2, cb) {
+  return cb(param1, param2);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,11 +18,26 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function greeting(firstname, lastname) {
+  return `Hello ${firstname} ${lastname}, nice to meet you!`
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // console.log(consume(2, 2, add)); // 4
 // console.log(consume(10, 16, multiply)); // 160
 // console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,6 +45,11 @@
 // Explain in your own words why nestedfunction can access the variable internal.
 
 // Explanation: 
+
+/*There is a box. Inside that box there are 2 smaller boxes. The myfunction box belongs to the main box which is the 
+'const extrernal' box only, but the 'nested function' box belongs to both 'myfunction box' and the 'const external'. 
+The problem is the nested function box does not know what internal means. 
+So it decides to look outside the box and figures out it belongs to the external box.*/
 
 
 const external = "I'm outside the function";
